e4a504ddb7ec0e1a4089d17ffd776073b3c1aeb2
george <beldenge@gmail.com>
2015-01-31 16:38:33 -0500

    Updated database locale for cross-platform compatibility.

--- sql/ZodiacCipher.sql
+++ sql/ZodiacCipher.sql
@@ -25,8 +25,8 @@ CREATE DATABASE "Zodiac"
   WITH OWNER = postgres
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
-       LC_COLLATE = 'English, United States'
-       LC_CTYPE = 'English, United States'
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8'
        CONNECTION LIMIT = -1;
 	   
 -- Table: parts_of_speech
