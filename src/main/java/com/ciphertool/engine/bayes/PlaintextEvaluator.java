c0b30adc91677031ba6ee5eb72810b2d1a1007c0
george <george@10.0.0.102>
2017-01-19 10:05:00 -0500

    Fix spelling

--- src/main/java/com/ciphertool/engine/bayes/PlaintextEvaluator.java
+++ src/main/java/com/ciphertool/engine/bayes/PlaintextEvaluator.java
@@ -129,7 +129,7 @@ public class PlaintextEvaluator {
 		for (WordProbability word : words) {
 			nGramProbability = null;
 
-			// Loop over all indices so that non-matching indices negatively affect the probabitliy
+			// Loop over all indices so that non-matching indices negatively affect the probability
 			for (int i = 0; i <= word.getValue().length() - order; i++) {
 				match = letterMarkovModel.findLongest(word.getValue().substring(i, i + order));
 
